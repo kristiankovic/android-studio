9a030e8d210510bd86e627f0f024d77f227c9970
kristiankovic <vv23011@ues.edu.sv>
2026-03-04 17:33:14 -0600

    uso de clases como parametros

--- arrays/app/src/main/java/com/pdm/arrays/MainActivity.java
+++ arrays/app/src/main/java/com/pdm/arrays/MainActivity.java
@@ -1,8 +1,12 @@
 package com.pdm.arrays;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,14 +14,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.pdm.arrays.models.Departamento;
+
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
-    // spiners -> cada posicion del spiner retorna algo
+    // spiners -> cada posicion del spiner retorna algo y funciona como un indice
 
     public Spinner spinnerDep;
     public Spinner spinnerDep1;
+    public ArrayList<String> listaNombres;
+
+    // arraylist q almacena obj departamentos
+    public ArrayList<Departamento> dataDep;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +39,10 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
+        dataDep = new ArrayList<>();
+        dataDep.add(new Departamento("101", "San Miguel"));
+        dataDep.add(new Departamento("102", "La Union"));
+
         // vivo con el bug
         spinnerDep = findViewById(R.id.spDep);
 
@@ -36,19 +51,48 @@ public class MainActivity extends AppCompatActivity {
         data.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         // adapters ->
-        spinnerDep.setAdapter(data);
+        //spinnerDep.setAdapter(data);
+
 
         // usando el constructor
-        ArrayList<String> listaNombres = new ArrayList<>();
+        listaNombres = new ArrayList<>();
 
         // asignacion de datos
         listaNombres.add("Uluazapa");
-        listaNombres.add("Uluazapa");
-        listaNombres.add("Uluazapa");
-        listaNombres.add("Uluazapa");
+        listaNombres.add("Guazapa");
+        listaNombres.add("San Bartolo");
+        listaNombres.add("La Perla");
 
         // adaptador
-        ArrayAdapter<String> datos = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, listaNombres);
-        spinnerDep1.setAdapter(datos);
+        //ArrayAdapter<String> datos = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, listaNombres);
+
+        ArrayAdapter<Departamento> datos = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, dataDep);
+        datos.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerDep.setAdapter(datos);
+
+        // sale del contexto de la app y entra en la de layout
+        spinnerDep.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                // captura el indice del objeto seleccionado
+                //Toast.makeText(MainActivity.this, "Indice" +i, Toast.LENGTH_SHORT).show();
+                //String selectedIndex = adapterView.getItemAtPosition(i).toString();
+
+                // otras formas para hacerlo
+                //String selectedIndex = listaNombres.get(i);
+                //String selectedIndex2 = spinnerDep.getSelectedItem().toString();
+
+                // para capturar todo el objeto
+                Departamento dep = (Departamento)adapterView.getItemAtPosition(i);
+
+                Toast.makeText(MainActivity.this, "Codigo Departamento: " + dep.getNombreDepa(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
