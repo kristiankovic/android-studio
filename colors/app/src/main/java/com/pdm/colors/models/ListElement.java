8073de8f90c67289ffa216f534685289759c084d
kristiankovic <vv23011@ues.edu.sv>
2026-04-19 16:07:39 -0600

    mejoras en la logica de acceder al objeto

--- colors/app/src/main/java/com/pdm/colors/models/ListElement.java
+++ colors/app/src/main/java/com/pdm/colors/models/ListElement.java
@@ -1,6 +1,8 @@
 package com.pdm.colors.models;
 
-public class ListElement {
+import java.io.Serializable;
+
+public class ListElement implements Serializable {
 
     public String color;
     public String name;
